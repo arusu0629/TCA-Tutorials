951f0b01e7ce847d5a2ae95a0157270be6e1e91d
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2023-07-24 18:34:42 +0900

    Add Initial Project

---
+++ TCA-Tutorials/TCA-Tutorials/TCA_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TutorialsApp.swift
+//  TCA-Tutorials
+//
+//  Created by af_nakandakari_toru on 2023/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
