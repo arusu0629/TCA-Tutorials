d361d18b42fd27d30bbd6ffbe3d130253e2d64d0
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2023-07-25 15:51:38 +0900

    [Section 3] Integrating into the app

--- TCA-Tutorials/TCA-Tutorials/TCA_TutorialsApp.swift
+++ TCA-Tutorials/TCA-Tutorials/TCA_TutorialsApp.swift
@@ -5,13 +5,19 @@
 //  Created by af_nakandakari_toru on 2023/07/24.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TCA_TutorialsApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store: TCA_TutorialsApp.store)
         }
     }
 }
