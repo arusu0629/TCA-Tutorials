a6f1660409412a562f55820a0a568db8ff87af22
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2023-07-25 19:41:40 +0900

    [Section 2] Performing a network request

--- TCA-Tutorials/TCA-Tutorials/CounterFeature.swift
+++ TCA-Tutorials/TCA-Tutorials/CounterFeature.swift
@@ -19,6 +19,7 @@ struct CounterFeature: ReducerProtocol {
     enum Action {
         case decrementButtonTapped
         case factButtonTapped
+        case factResponse(String)
         case incrementButtonTapped
     }
 
@@ -33,14 +34,15 @@ struct CounterFeature: ReducerProtocol {
             state.fact = nil
             state.isLoading = true
 
-            let (data, _) = try await URLSession.shared
-                .data(from: URL(string: "http://numbersapi.com/\(state.count)")!)
-                // 🛑 'async' call in a function that does not support concurrency
-                // 🛑 Errors thrown from here are not handled
+            return .run { [count = state.count] send in
+                let (data, _) = try await URLSession.shared.data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                let fact = String(decoding: data, as: UTF8.self)
+                await send(.factResponse(fact))
+            }
 
-            state.fact = String(decoding: data, as: UTF8.self)
+        case let .factResponse(fact):
+            state.fact = fact
             state.isLoading = false
-
             return .none
 
         case .incrementButtonTapped:
