b772dc28f55f76b8eadc46553aa85da474047686
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2023-07-27 12:01:39 +0900

    [Your first presentation] [Section 1] Project set up

---
+++ TCA-Tutorials/TCA-Tutorials/ContactsExample/ContactsFeature.swift
@@ -0,0 +1,76 @@
+//
+//  ContactsFeature.swift
+//  TCA-Tutorials
+//
+//  Created by af_nakandakari_toru on 2023/07/27.
+//
+
+import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct Contact: Equatable, Identifiable {
+    let id: UUID
+    var name: String
+}
+
+struct ContactsFeature: ReducerProtocol {
+    struct State: Equatable {
+        var contacts: IdentifiedArrayOf<Contact> = []
+    }
+    enum Action: Equatable {
+        case addButtonTapped
+    }
+    var body: some ReducerProtocolOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .addButtonTapped:
+                // TODO: Handle action
+                return .none
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    let store: StoreOf<ContactsFeature>
+
+    var body: some View {
+        NavigationStack {
+            WithViewStore(self.store, observe: \.contacts) { viewStore in
+                List {
+                    ForEach(viewStore.state) { contact in
+                        Text(contact.name)
+                    }
+                }
+                .navigationTitle("Contacts")
+                .toolbar {
+                    ToolbarItem {
+                        Button {
+                            viewStore.send(.addButtonTapped)
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(
+            store: Store(
+                initialState: ContactsFeature.State(
+                    contacts: [
+                        Contact(id: UUID(), name: "Blob"),
+                        Contact(id: UUID(), name: "Blob Jr"),
+                        Contact(id: UUID(), name: "Blob Sr"),
+                    ]
+                ),
+                reducer: ContactsFeature()
+            )
+        )
+    }
+}
